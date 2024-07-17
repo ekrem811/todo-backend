6a880f522ab8e1fb98189cf66d43e42155394f95
ekrem811 <ekrem.elbasan.11@gmail.com>
2024-07-17 13:56:05 +0300

    username not found exception handled

--- src/main/java/com/group/todo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/group/todo/config/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -55,6 +56,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             filterChain.doFilter(request, response);
         } catch (SignatureException e) {
+            response.setStatus(401);
+        } catch (UsernameNotFoundException e) {
             response.setStatus(401);
         }
     }
